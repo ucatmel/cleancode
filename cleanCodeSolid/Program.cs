41cfe0fbb8bdfd06cd2b76f32ccf88b6671a3a5e
ucatmel <19824600+ucatmel@users.noreply.github.com>
2018-07-09 21:03:56 +0200

    Basic RestClient with Demo Data

--- cleanCodeSolid/Program.cs
+++ cleanCodeSolid/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using RestSharp;
 
 namespace cleanCodeSolid
 {
@@ -6,8 +7,15 @@ namespace cleanCodeSolid
     {
         static void Main(string[] args)
         {
-            
+            var client = new RestClient();
+            client.BaseUrl = new Uri("http://api.openweathermap.org");
+
+            var request = new RestRequest();
+            request.Resource = "data/2.5/weather?q=Wuppertal&appid=8124eedeb9bba16c11f17307ad080d6a";
+
+            IRestResponse response = client.Execute(request);
             Console.WriteLine("Hello World!");
+            Console.WriteLine(response.Content);
             Console.ReadKey();
 
         }
