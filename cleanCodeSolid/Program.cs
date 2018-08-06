90dced4b7e8796b32e925e84f99f0ac5fdca4b1e
Andi <ucatmel@gmx.net>
2018-08-06 08:30:37 +0200

    Load WeatherData

--- cleanCodeSolid/Program.cs
+++ cleanCodeSolid/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using RestSharp;
+using Newtonsoft.Json.Converters;
 
 namespace cleanCodeSolid
 {
@@ -7,14 +8,25 @@ namespace cleanCodeSolid
     {
         static void Main(string[] args)
         {
+            string stadt = "";
             var client = new RestClient();
             client.BaseUrl = new Uri("http://api.openweathermap.org");
 
             var request = new RestRequest();
-            request.Resource = "data/2.5/weather?q=Wuppertal&appid=8124eedeb9bba16c11f17307ad080d6a";
+            var weatherQuery = "data/2.5/weather?q=";
+            var appID = "&appid=8124eedeb9bba16c11f17307ad080d6a";
+            var lang = "&lang=de";
+            
+
+            
+            Console.WriteLine("Von welcher Stadt soll das Wetter angezeigt werden:");
+            stadt = Console.ReadLine();
+            request.Resource = $"{weatherQuery}{stadt}{appID}{lang}";
 
             IRestResponse response = client.Execute(request);
-            Console.WriteLine("Hello World!");
+            var rootweather = Newtonsoft.Json.JsonConvert.DeserializeObject<RootObject>(response.Content);
+
+            Console.WriteLine($"Das Wetter in {stadt} ist {rootweather.weather[0].description}" );
             Console.WriteLine(response.Content);
             Console.ReadKey();
 
